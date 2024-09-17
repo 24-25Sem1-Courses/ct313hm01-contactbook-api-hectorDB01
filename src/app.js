e4969bcf65a0a2377dde7b27b0ac94eb1f531780
DBN <ng.db03@gmail.com>
2024-09-17 10:12:07 +0700

    Server static files

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ const cors = require('cors')
 const JSend=require('./jsend')
 const contactsRouter = require('./routes/contacts.router');
 
+const {
+    resourceNotFound,
+    handleError,
+} = require('./controllers/errors.controller')
+
+const {specs, swaggerUi} = require('./docs/swagger')
+
 const app = express()
 
 app.use(cors())
@@ -16,5 +23,15 @@ app.get('/', (req, res) => {
     )
 })
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+app.use('/public', express.static('public'))
+
 contactsRouter.setup(app);
+
+// Handle 404 response
+app.use(resourceNotFound)
+
+// Define error-handling middleware last, after other app.use() and routes calls
+app.use(handleError)
+
 module.exports = app
